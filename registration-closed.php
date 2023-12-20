0b56ca8e10949148b7792d2cc0c6457e6d70564d
MarcelVlcek <vlcek.marc@gmail.com>
2023-12-20 22:21:44 +0100

    Prvý commit

---
+++ registration-closed.php
@@ -0,0 +1,10 @@
+<?php include 'shared/header.php'; ?>
+
+<main style="display: flex; align-items: center; justify-content: center; height: 50vh;">
+    <h1 style="color: red; font-size: 20px; text-align: center;">
+       Registrácia bude spustená 1. mája
+    </h1>
+    <section></section>
+</main>
+
+<?php include 'shared/footer.php'; ?>
